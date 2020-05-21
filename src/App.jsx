2dd5bb6b60274e736283c84a89e1eb9b4400f2e2
annepattrice <58075538+annepattrice@users.noreply.github.com>
2020-05-21 18:23:34 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,57 @@ import "./App.scss";
 class App extends React.Component {
   state = {
     contacts: [],
+    search: "",
+    filteredContacts: [],
+    sortSelected: "",
   };
   async componentDidMount() {
     const data = await Api.getContacts();
 
     this.setState({
       contacts: data,
+      filteredContacts: data,
     });
   }
+  handleSearch = (e) => {
+    e.preventDefault();
+    const filteredContacts = this.state.contacts.filter((contact) =>
+      contact.name.toLowerCase().includes(e.target.value.toLowerCase())
+    );
+    this.setState({ filteredContacts });
+  };
+
+  handleSort = (e) => {
+    const { filteredContacts } = this.state;
+    const {
+      target: { name },
+    } = e;
+    this.setState({ sortSelected: name });
+
+    const compareFilter = (a, b) => {
+      const x = typeof a[name] === "string" ? a[name].toLowerCase() : a[name];
+      const y = typeof b[name] === "string" ? b[name].toLowerCase() : b[name];
+
+      return x < y ? -1 : x > y ? 1 : 0;
+    };
+
+    const dataFiltered = filteredContacts.sort(compareFilter);
+
+    this.setState({ filteredContacts: dataFiltered });
+  };
 
   render() {
+    const { sortSelected, filteredContacts } = this.state;
     return (
       <>
         <Topbar />
-        <Filters />
-
-        <Contacts contacts={this.state.contacts} />
+        <Filters
+          search={this.search}
+          handleSearch={this.handleSearch}
+          handleSort={this.handleSort}
+          buttonSortSelected={sortSelected}
+        />
+        <Contacts contacts={filteredContacts} />
       </>
     );
   }
